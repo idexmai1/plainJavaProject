b458bb28d4f4d25a1e2beec5a8f5eb9dda1b417e
idexmai1 <iedexmain1@gmail.com>
2016-05-16 17:56:46 +0300

    Update MainClass.java

--- src/com/company/nba/MainClass.java
+++ src/com/company/nba/MainClass.java
@@ -1,14 +1,10 @@
 package com.company.nba;
 
-
-import org.eclipse.che.examples.GreetingController;
-
 public class MainClass {
 
     public static void main(String[] args) {
 	// write your code here
 	String string = "I love this game!";
 	System.out.println(string);
-	GreetingController greet = new GreetingController();
     }
 }
